ccd68b047aba4abb04873c5cff519ae63872b10d
Lewis Mcadam <lewismcadam5@gmail.com>
2019-01-25 20:53:34 +0000

    Base Game
    placeholder movement

---
+++ DisorderGame/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody2D body;
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+    public float runSpeed = 20;
+
+	
+	void Start () {
+
+        body = GetComponent<Rigidbody2D>();
+
+	}
+	
+	void Update () {
+
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+	}
+
+    void FixedUpdate ()
+    {
+
+        if(horizontal != 0 && vertical != 0)
+        {
+            body.velocity = new Vector2((horizontal * runSpeed) * moveLimiter, (vertical * runSpeed) * moveLimiter);
+        }
+        else
+        {
+            body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+        }
+
+    }
+}
